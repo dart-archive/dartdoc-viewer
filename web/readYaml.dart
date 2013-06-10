476ef4d99ce45b3553ea51604009073fde2a22d5
Janice Leung <jjw1707@gmail.com>
2013-06-10 14:59:03 -0700

    replaced generatePage and generateCategory with generateContent

--- web/readYaml.dart
+++ web/readYaml.dart
@@ -15,49 +15,45 @@ List<Page> loadData(response) {
   var doc = loadYaml(response);
   List<Page> pageList = new List<Page>();
   for (String k in doc.keys) {
-    Page page = generatePage(k, doc[k]);
+    //Page page = generatePage(k, doc[k]);
+    Page page = generateContent(k, doc[k], true);
     pageList.add(page);
   }
   return pageList;
 }
 
 /**
- *  Returns a page with a list of categories from a map of category. 
+ * Returns either a Page or Category depending on what is needed. 
+ * 
+ * needPage should be true if a page is needed, 
+ * and false if a category is needed. 
  */
-Page generatePage(String name, Map<String, String> pageMap) {
-  List<Category> categories = new List<Category>();
-  for (String k in pageMap.keys) {
-    Category category;
-    // To check whether there is another map contained inside. 
-    // If there is another map, it is necessary to generate a list of items. 
-    if (pageMap[k].toString()[0] == '{') {
-      category = generateCategory(k, pageMap[k]);
+generateContent(String name, Map<String, String> map, bool needPage) {
+  List subContentList = new List();
+  for(var k in map.keys){
+    var subContent;
+    if (map[k] is Map) {
+      if (needPage) {
+        subContent = generateContent(k, map[k], false);
+      } else {
+        subContent = new CategoryItem.withPage(k, 
+            generateContent(k, map[k], true));
+      }
     } else {
-      category = new Category(k);
+      if (needPage) {
+        subContent = new Category(k);
+      } else {
+        subContent = new CategoryItem(k);
+      }
     }
-    categories.add(category);
+    subContentList.add(subContent);
   }
-  Page page = new Page.withCategories(name, categories);
-  pageIndex[name] = page;
-  return page;
-}
-
-/**
- *  Returns a category with a list of items from a map of items. 
- */
-Category generateCategory(String name, Map<String, String> categoryMap) {
-  List<CategoryItem> items = new List<CategoryItem>();
-  for (String k in categoryMap.keys) {
-    CategoryItem item;
-    // To check whether there is another map contained inside. 
-    // If there is another map, it is necessary to generate another page. 
-    if (categoryMap[k].toString()[0] == '{') {
-      item = new CategoryItem.withPage(k, generatePage(k, categoryMap[k]));
-    } else {
-      item = new CategoryItem(k);
-    }
-    items.add(item);
+  var content;
+  if (needPage) {
+    content = new Page.withCategories(name, subContentList);
+    pageIndex[name] = content;
+  } else {
+    content = new Category.withItems(name, subContentList);
   }
-  Category category = new Category.withItems(name, items);
-  return category;
+  return content;
 }
