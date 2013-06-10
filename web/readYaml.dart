23cb822a499765259037df522f1ba9a3bdc84543
Janice Leung <jjw1707@gmail.com>
2013-06-10 12:51:04 -0700

    Removed debug print statements

--- web/readYaml.dart
+++ web/readYaml.dart
@@ -14,11 +14,7 @@ Future getHttp() {
 
 Page loadData(response) {
   var doc = loadYaml(response);
-  print(doc);
   for (String k in doc.keys) {
-    print(k);
-    print(doc[k]);
-    
     page = generatePage(k, doc[k]);
     return page;
   }
@@ -28,12 +24,8 @@ Page loadData(response) {
  *  Returns a page with a list of categories from a map of category. 
  */
 Page generatePage(String name, Map<String, String> pageMap) {
-  
   List<Category> categories = new List<Category>();
   for (String k in pageMap.keys) {
-    print(k);
-    print(pageMap[k]);
-    
     Category category;
     // To check whether there is another map contained inside. 
     // If there is another map, it is necessary to generate a list of items. 
@@ -52,12 +44,8 @@ Page generatePage(String name, Map<String, String> pageMap) {
  *  Returns a category with a list of items from a map of items. 
  */
 Category generateCategory(String name, Map<String, String> categoryMap) {
-  
   List<CategoryItem> items = new List<CategoryItem>();
   for (String k in categoryMap.keys) {
-    print(k);
-    print(categoryMap[k]);
-    
     CategoryItem item;
     // To check whether there is another map contained inside. 
     // If there is another map, it is necessary to generate another page. 
