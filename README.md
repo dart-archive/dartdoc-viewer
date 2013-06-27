ae817e90d4ad43e2d76afe003d94e6e65544c7b1
tmandel@google.com <tmandel@google.com>
2013-06-26 17:41:16 -0700

    Added run.sh for drone and command line testing

--- README.md
+++ README.md
@@ -1,4 +1,7 @@
 dartdoc-viewer
 ==============
 
+The status of our tests in the test/ folder:
+[![Build Status](https://drone.io/github.com/dart-lang/dartdoc-viewer/status.png)](https://drone.io/github.com/dart-lang/dartdoc-viewer/latest)
+
 dartdoc-viewer
---
+++ test/run.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+# Copyright (c) 2013, the Dart project authors.  Please see the AUTHORS file
+# for details. All rights reserved. Use of this source code is governed by a
+# BSD-style license that can be found in the LICENSE file.
+
+# Usage: call directly in the command line as test/run.sh with 'content_shell' 
+# in your path. This script runs all tests for the dartdoc viewer.
+
+results=$(content_shell --dump-render-tree test/viewer_test.html)
+echo $results
+
+echo $results | grep -q PASS
+passed=$?
+
+echo $results | grep -q FAIL
+failures=$?
+
+echo $results | grep -q ERROR
+errors=$?
+
+if [[ ($cs_exit_code != 0 || $failures == 0 || 
+       $errors == 0 || $passed != 0) ]]
+then
+    exit 0
+fi
+
+exit 1
