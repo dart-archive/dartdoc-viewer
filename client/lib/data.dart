2b414d78d82842957a947f240255a4fa501e3bd4
tmandel@google.com <tmandel@google.com>
2013-08-09 14:09:17 -0700

    Added more information to search results

--- client/lib/data.dart
+++ client/lib/data.dart
@@ -11,7 +11,7 @@ Map<String, Item> pageIndex = toObservable({});
 
 // Since library names can contain '.' characters, they must be mapped to
 // a new form for linking purposes. This maps original library names to names
-// with '%' characters replacing the '.' characters for consistency.
+// with '-' characters replacing the '.' characters for consistency.
 Map<String, String> libraryNames = {};
 
 String findLibraryName(String type) {
